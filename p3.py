268c6f3f41f4b76f758b9ff86cbeca83eeacff97
xoadul <abdulroshan901@gmail.com>
2025-04-19 03:52:52 -0700

    initial commit

--- p3.py
+++ p3.py
@@ -1,1 +1,54 @@
-print ("i like anime!")
+# this is my first python program
+print ('this is me roshan')
+print ("i study in 12th class")
+print ("i go to collage on car")
+print (" i like chocolate")
+print ("ur so pretty")
+# strings 
+first_name="roshan"
+
+print (f"hello {first_name}")
+food="rice and curry"
+print(f"i like {food}")
+sports="footall"
+print(f"my fav sport is {sports}")
+email="sis123@light.com"
+print (f"my email is {email}")
+                             # the end 
+
+# integers
+age=18
+quantity=4
+
+print (f"you are selling {quantity} in your shop")
+print (f"your age is {age} yearas old")
+
+                              #the end
+
+ #float
+ #float contains decimals like
+
+price=9.99
+distance=6.5
+print (f"the product price is ${price}")
+print (f" in morning i ran {distance}km")
+                               #the end
+
+
+#boolean
+#it is basically True or False 
+
+
+for_sale=False
+
+if for_sale:
+    print("that car is for sale")
+else:
+    print ("that car is not for sale")
+
+is_online=False
+
+if is_online:
+    print ("ur online in insta")
+else:
+    print ("ur not online in insta")
